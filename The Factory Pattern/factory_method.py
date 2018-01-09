64b853a60c14c4fff329ab20e4e6a4c7e5986ebd
Kritsana <kritsana.chai@ku.th>
2018-01-09 23:59:41 +0700

    Add main medthod

--- The Factory Pattern/factory_method.py
+++ The Factory Pattern/factory_method.py
@@ -37,3 +37,25 @@ def connect_to(filepath):
     except ValueError as ve:
         print(ve)
     return factory
+
+def main():
+    sqlite_factory = connect_to('person.sq3')
+
+    xml_factory = connect_to('person.xml')
+    xml_data = xml_factory.parsed_data()
+    liars = xml_data.findall(".//{person}[{lastName}='{}']".format('Liar'))
+    print('found: {} person'.format(len(liars)))
+    for liar in liars:
+        print('first name: {}'.format(liar.find('firstName').text))
+        print('last name: {}'.format(liar.find('lastName').text))
+        [print('phone number ({})'.format(p.attrib['type']),
+                p.text) for p in liar.find('phoneNumbers')]
+
+    json_factory = connect_to('donut.json')
+    json_data = json_factory.parsed_data
+    print('found: {} donuts'.format(len(json_data)))
+    for donut in json_data:
+        print('name: {}'.format(donut['name']))
+        print('price: ${}'.format(donut['ppu']))
+        [print('topping: {} {}'.format(t['id'], t['type'])
+                for t in donut['topping'])]
