10728e38794288d80f13ace7f4f9631e67105103
Kritsana <kritsana.chai@ku.th>
2018-01-09 23:36:01 +0700

    Add factory_medthod file, add JSONConnector class

---
+++ The Factory Pattern/factory_method.py
@@ -0,0 +1,12 @@
+import xml.etree.ElementTree as etree
+import json
+
+class JSONConnector:
+    def __init__(self, filepath):
+        self.data = dict()
+        with open(filepath, mode='r', encoding='utf-8') as f:
+            self.data = json.load(f)
+
+    @property
+    def parsed_data(self):
+        return self.data
