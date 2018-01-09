4fb51924e54209016ee5cfbcf08667d874eced05
Kritsana <kritsana.chai@ku.th>
2018-01-09 23:38:29 +0700

    Add XMLConnector class

--- The Factory Pattern/factory_method.py
+++ The Factory Pattern/factory_method.py
@@ -2,6 +2,7 @@ import xml.etree.ElementTree as etree
 import json
 
 class JSONConnector:
+
     def __init__(self, filepath):
         self.data = dict()
         with open(filepath, mode='r', encoding='utf-8') as f:
@@ -10,3 +11,12 @@ class JSONConnector:
     @property
     def parsed_data(self):
         return self.data
+
+class XMLConnector:
+
+    def __init__(self, filepath):
+        self.tree = etree.parse(filepath)
+
+    @property
+    def parsed_data(self):
+        return self.tree
